b8c44e7bd0b7ebb30e58258914c680ed4d5fc157
Gaurav Mourya <gauravmouryaweb@gmail.com>
2017-12-15 10:55:36 +0530

    file.php

---
+++ file.php
@@ -0,0 +1,14 @@
+<?php
+
+$_product=protected object;
+  $data="data";
+			 $array = (array)$_product;
+			  $prefix = chr(0).'*'.chr(0);
+			  
+			var_dump($array[$prefix.$data]);die;
+      
+      
+      
+  
+  
+?>
